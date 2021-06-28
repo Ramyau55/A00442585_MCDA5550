aaab2ea21a107567b8729fcc52c6d74280a34185
Ramya Sekar <ramya.sekar@smu.ca>
2021-06-28 15:10:18 -0300

    pushing the android app

---
+++ AndroidStudioProjects/HotelReservation/app/src/main/java/com/example/hotelreservation/HotelSearchFragment.java
@@ -0,0 +1,50 @@
+package com.example.hotelreservation;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class HotelSearchFragment extends Fragment {
+    View view;
+    TextView titleTextView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.hotel_search_layout,container,false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        titleTextView = view.findViewById(R.id.title_txt_view);
+        titleTextView.setText(R.string.title_txt);
+    }
+
+    // Function to get the date object
+    private String getDateFromCalendar(DatePicker datePicker) {
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String formattedDate = simpleDateFormat.format(calendar.getTime());
+
+        return formattedDate;
+    }
+}
